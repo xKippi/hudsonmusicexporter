d48d12ccbf17d8f4b5cc1a0851d0aec5bbccd394
xKippi <daniel.kipp@snorre.com>
2021-09-26 11:47:07 +0200

    Added file picker and command line options, general imporvements

--- hudsonmusicexporter.py
+++ hudsonmusicexporter.py
@@ -1,17 +1,35 @@
 #!/bin/python3
-import time
 
+######################################################
+# hudsonmusicexporter
+# Script to export books from the Hudson Music Digital Library as PDF
+#
+# Author: Kippi
+# Version: 1.0
+######################################################
+
+# --- BEGIN IMPORTS (DO NOT EDIT) ---
+import argparse
 import keyboard
-import os
 import time
-from pathlib import Path
 from PIL import Image, ImageGrab
+from tkinter import Tk
+from tkinter.filedialog import asksaveasfilename
+# --- END IMPORTS
+
+# --- BEGIN CONFIG ---
+
+zoomed = True
+
+zoom = 1.05
+
+output_file = None
+
+# --- END CONFIG ---
+
+# DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
 
 VERSION = "1.0"
-ZOOMED = True
-ZOOM = 1.05
-ZOOM_CONSTANTS = (767.65, 1023.81)  # Determined by linear regression
-OUTPUT_FILE = os.path.join(str(Path.home()), "Documents", "book.pdf")
 
 DEFAULT_SIZES = [
     (735 / 1920, 980 / 1080),   # RELATIVE_PAGE_SIZE
@@ -19,69 +37,112 @@ DEFAULT_SIZES = [
     (225 / 1920, 50 / 1080),    # DOUBLE_PAGE_RELATIVE_OFFSET
 ]
 
-ZOOMED_SIZES = [ (round(ZOOM * ZOOM_CONSTANTS[0]) / 1920, round(ZOOM * ZOOM_CONSTANTS[1]) / 1080) ]
-ZOOMED_SIZES.append((round((1920 - ZOOMED_SIZES[0][0] * 1920) / 2) / 1920, round((1080 - ZOOMED_SIZES[0][1] * 1080) / 2) / 1080))
-ZOOMED_SIZES.append((round((1920 - ZOOMED_SIZES[0][0] * 2 * 1920) / 2) / 1920, ZOOMED_SIZES[1][1]))
+ZOOM_CONSTANTS = (767.65, 1023.81)  # Determined by linear regression
 
 
-def capture(images, offset, page_size):
-    global count
-    images.append(ImageGrab.grab(bbox=(offset[0], offset[1], offset[0] + page_size[0], offset[1] + page_size[1])))
-    print("Captured page " + str(count))
-    count += 1
+def main():
+    global zoomed, zoom, output_file
+
+    def capture(images, offsets, page_size, count):
+        time.sleep(2)
+        for offset in offsets:
+            images.append(ImageGrab.grab(bbox=(offset[0], offset[1], offset[0] + page_size[0], offset[1] + page_size[1])))
+            print("Captured page " + str(count[0]))
+            count[0] += 1
+        keyboard.send("right")
+
+    parser = argparse.ArgumentParser(description='Export books from the Hudson Music Digital Library')
+    parser.add_argument('-z',
+                        '--zoom',
+                        type=int,
+                        action='store',
+                        dest='zoom',
+                        default=None,
+                        required=False,
+                        help='Specify the zoom level.')
+    parser.add_argument('-o',
+                        '--output-file',
+                        type=str,
+                        action='store',
+                        dest='filename',
+                        required=False,
+                        default=None,
+                        help='Specify where the book should be saved. If not specified or invalid a file picker dialog will appear.')
+
+    args = parser.parse_args()
+
+    if args.zoom is not None:
+        zoom = args.zoom
+        zoomed = True
+
+    if args.filename is not None:
+        output_file = args.filename
+
+    screenshot = ImageGrab.grab()
+
+    zoomed_sizes = [(round(zoom * ZOOM_CONSTANTS[0]) / 1920, round(zoom * ZOOM_CONSTANTS[1]) / 1080)]
+    zoomed_sizes.append((round((1920 - zoomed_sizes[0][0] * 1920) / 2) / 1920, round((1080 - zoomed_sizes[0][1] * 1080) / 2) / 1080))
+    zoomed_sizes.append((round((1920 - zoomed_sizes[0][0] * 2 * 1920) / 2) / 1920, zoomed_sizes[1][1]))
+
+    sizes = zoomed_sizes if zoomed else DEFAULT_SIZES
+    page_size = (sizes[0][0] * screenshot.size[0], sizes[0][1] * screenshot.size[1])
+    single_page_offset = (sizes[1][0] * screenshot.size[0], sizes[1][1] * screenshot.size[1])
+    double_page_offset = [(sizes[2][0] * screenshot.size[0], sizes[2][1] * screenshot.size[1])]
+    double_page_offset.append((double_page_offset[0][0] + page_size[0] + 1, double_page_offset[0][1]))
+
+    welcome_text = "Welcome to HusomMusicExporter v"+str(VERSION)
+    print(welcome_text)
+    print("-" * len(welcome_text))
+    print()
+
+    page_count = 0
+    while page_count < 1:
+        try:
+            page_count = int(input("Please tell me how many pages the book has: "))
+        except ValueError:
+            pass
+
+    print()
+    print("To export your book do the following:")
+    print(" - Open the Hudson Music application")
+    print(" - Find the book you want to export and press \"Read\"")
+    print(" - Navigate to the first page of the book")
+    print(" - Set the zoom to 105% (bottom left corner)")
+    print(" - Go into fullscreen mode")
+    print(" - Press [Ctrl] + [Enter]")
+    print(" - Wait until the program completes")
+    print()
+    print()
+
+    count = [1]  # We want to pass this by reference
+    images = []
+    odd_page_count = page_count - (page_count+1) % 2
+    keyboard.wait("ctrl+enter")
+
+    capture(images, [single_page_offset], page_size, count)
+
+    while count[0] < odd_page_count:
+        capture(images, double_page_offset, page_size, count)
+
+    if odd_page_count != page_count:
+        capture(images, [single_page_offset], page_size, count)
+
+    Tk().withdraw()
+    if output_file is None:
+        output_file = asksaveasfilename(defaultextension='.pdf', filetypes=[("PDF Documents", '*.pdf')], title="Save the book as...")
+
+    while True:
+        try:
+            with open(output_file, "w") as f:
+                images.pop(0).save(output_file, "PDF", resolution=100.0, save_all=True, append_images=images)
+                print("Creating PDF as \"" + output_file + "\"...")
+                break
+        except OSError:
+            print("The selected file was not valid, please try again")
+            output_file = asksaveasfilename(defaultextension='.pdf', filetypes=[("PDF Documents", '*.pdf')], title="Save the book as...")
+
+    print("Done!")
 
 
-screenshot = ImageGrab.grab()
-
-sizes = ZOOMED_SIZES if ZOOMED else DEFAULT_SIZES
-page_size = (sizes[0][0] * screenshot.size[0], sizes[0][1] * screenshot.size[1])
-single_page_offset = (sizes[1][0] * screenshot.size[0], sizes[1][1] * screenshot.size[1])
-double_page_offset = [(sizes[2][0] * screenshot.size[0], sizes[2][1] * screenshot.size[1])]
-double_page_offset.append((double_page_offset[0][0] + page_size[0] + 1, double_page_offset[0][1]))
-
-welcome_text = "Welcome to HusomMusicExporter v"+str(VERSION)
-print(welcome_text)
-print("-" * len(welcome_text))
-print()
-
-page_count = 0
-while page_count < 1:
-    try:
-        page_count = int(input("Please tell me how many pages the book has: "))
-    except ValueError:
-        pass
-
-print()
-print("To export your book do the following:")
-print(" - Open the Hudson Music application")
-print(" - Find the book you want to export and press \"Read\"")
-print(" - Navigate to the first page of the book")
-print(" - Set the zoom to 105% (bottom left corner)")
-print(" - Go into fullscreen mode")
-print(" - Press [Ctrl] + [Enter]")
-print(" - Wait until the program completes")
-print()
-print()
-
-count = 1
-images = []
-odd_page_count = page_count - (page_count+1) % 2
-keyboard.wait("ctrl+enter")
-
-capture(images, single_page_offset, page_size)
-
-while count < odd_page_count:
-    keyboard.send("right")
-    time.sleep(2)
-    capture(images, double_page_offset[0], page_size)
-    capture(images, double_page_offset[1], page_size)
-
-if odd_page_count != page_count:
-    keyboard.send("right")
-    time.sleep(2)
-    capture(images, single_page_offset, page_size)
-
-print("Creating PDF as \"" + OUTPUT_FILE + "\"...")
-images.pop(0).save(OUTPUT_FILE, "PDF", resolution=100.0, save_all=True, append_images=images)
-
-print("Done!")
+if __name__ == '__main__':
+    main()
